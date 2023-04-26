043dfafd7fc16e1ce41bd3cfce29214bd91e62a7
zoalfikar <zoalfikaralsaad@gmail.com>
2023-04-26 22:14:35 +0300

    init

---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge } = require('electron')
+    // const vue = require('vue')
+    // console.log(vue);
+    // import { contextBridge } from "electron";
+    // import { createApp } from "vue";
+window.vueApp = vue
+    // contextBridge.exposeInMainWorld('Vue', {
+    //     createApp: (options) => console.log(options)
+    // })
